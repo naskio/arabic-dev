5f4ea35eee1755e22c9cc6a76a2f2088f342ec6d
Hanene Maafi <fh_maafi@esi.dz>
2019-04-20 21:28:20 +0100

    Add models

--- apps/stemmers_comparer/models.py
+++ apps/stemmers_comparer/models.py
@@ -1,7 +1,74 @@
 from django.db import models
+from star_ratings import get_star_ratings_rating_model_name
+from django.contrib.contenttypes.fields import GenericRelation
 
-# Create your models here.
 
-# TODO: add stemmer model
+# Author Model
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    github_account_link  =  models.CharField(max_length=255)
+    website = models.CharField(max_length=255)
 
-# TODO: add review model
+    def __str__(self):
+        return self.first_name
+
+
+# ProgrammingLanguage Model
+class ProgrammingLanguage(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
+    website = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+# Requirement Model
+class Requirement(models.Model):
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
+
+
+# Feature Model
+class Feature(models.Model):
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
+
+
+# Stemmer Model
+class Stemmer(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
+    display_name = models.CharField(max_length=50)
+    is_enabled = models.BooleanField(default=True)
+    authors = models.ManyToManyField(Author)
+    license = models.CharField(max_length=50, null=True)
+    description = models.TextField(null=True)
+    documentation_link = models.CharField(max_length=255, null=True)
+    download_link = models.CharField(max_length=255, null=True)
+    programming_languages = models.ManyToManyField(ProgrammingLanguage)
+    requirements = models.ManyToManyField(Requirement)
+    features = models.ManyToManyField(Feature)
+    how_to_use = models.TextField(null=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+       return self.display_name
+
+
+# Review Model
+class Review(models.Model):
+    stemmer = models.ForeignKey(Stemmer, on_delete=models.CASCADE)
+    ratings = GenericRelation(get_star_ratings_rating_model_name(), related_query_name='stemmers')
+    user_email_address = models.EmailField()
+    user_github_account_link = models.CharField(max_length=255, null=True)
+    comment = models.TextField()
+    comment_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.comment
